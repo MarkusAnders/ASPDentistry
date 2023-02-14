51dfc0bb3e1e05448fa27e52c27a21245dc26630
MarkusAnders <ser.play02@mail.ru>
2023-02-14 14:05:09 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPDentistry.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
